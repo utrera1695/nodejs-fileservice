d62756cb875d622c247ea42c49fa76b27eca5236
Victor <utrera1695@gmail.com>
2020-08-09 13:05:03 -0400

    test

--- service/file.service.js
+++ service/file.service.js
@@ -27,10 +27,25 @@ async function GetFile(req, res) {
     });
   }
 }
+
+function decodeBase64Image(dataString) {
+  var matches = dataString.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/);
+  var response = {};
+
+  if (matches.length !== 3) {
+    return new Error('Invalid input string');
+  }
+
+  response.type = matches[1];
+  response.data = new Buffer(matches[2], 'base64');
+
+  return response;
+}
 async function UploadFileBase64(req, res) {
   try {
     let base64 = req.body.base;
-    let imageTypeDetected = base64.type.match(/\/(.*?)$/);
+    var imageBuffer = decodeBase64Image(base64Data);
+    let imageTypeDetected = imageBuffer.type.match(/\/(.*?)$/);
     let base64Image = base64.split(';base64,').pop();
     let randomPart = Math.random().toString(36).substr(2, 5);
     fs.writeFile(
