bef00367ee271f60803dd4fada4164115ea7ef7a
victor <utrera1695@gmail.com>
2020-12-28 12:27:38 -0400

    no ssl

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 'use strict';
 var http = require('http')
-var https = require('https')
+/*var https = require('https')*/
 var app = require('./app');
-var fs = require('fs-extra')
+/*var fs = require('fs-extra')*/
 var port = process.env.PORT || 8000;
-var sslOptions = {
+/*var sslOptions = {
   key: fs.readFileSync('key.pem'),
   cert: fs.readFileSync('cert.pem')
-};
-/* var server = http.createServer(app);
-server.listen(port); */
+};*/
+var server = http.createServer(app);
+server.listen(port);
+/*
 console.log(sslOptions)
-https.createServer(sslOptions, app).listen(port)
+https.createServer(sslOptions, app).listen(port)*/
